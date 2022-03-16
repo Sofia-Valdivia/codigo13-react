c89ecbf6dc91eb03fa4e0907415e9f1b4e7578fe
Sofia-Valdivia <bastet.svt@gmail.com>
2022-03-15 23:02:33 -0500

    react-15Marzo

---
+++ src/Footer.js
@@ -0,0 +1,17 @@
+const Footer = () => {
+    return (
+      <div>
+        <footer>
+          <nav>
+            <ul>
+              <li>Facebook</li>
+              <li>Trip Advisor</li>
+              <li>Twitter</li>
+            </ul>
+          </nav>
+        </footer>
+      </div>
+    );
+  };
+  
+  export default Footer;
---
+++ src/pages/Home/index.js
@@ -0,0 +1,10 @@
+
+import { Container } from "@mui/material"
+const Home =()=>{
+    return(
+<Container>
+    <h1>Mi pagina en React</h1>
+</Container>
+    )
+}
+export default Home
